d347ef810f5d01f9733e561f77b5c04928cfbf84
Shubham Gadgil <155552779+DevShubhG@users.noreply.github.com>
2025-07-26 20:45:37 +0530

    Updated SparepartController.cs
    Added clear error message with ex.InnerException.Message

--- Inventory_WebAPI/Controllers/SparepartController.cs
+++ Inventory_WebAPI/Controllers/SparepartController.cs
@@ -36,9 +36,9 @@ namespace Inventory_WebAPI.Controllers
                 return Ok();
  
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.InnerException.Message);
             }
  
         }
@@ -61,9 +61,9 @@ namespace Inventory_WebAPI.Controllers
  
                 return Ok();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.InnerException.Message);
  
             }
  
